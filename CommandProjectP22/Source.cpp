f6ba28bc816ed7c0975aca80f296ab0f368793cd
Sergey Ivanov <mailto.sergeyivanov@gmail.com>
2023-06-14 15:45:04 +0300

    Добавил функцию LoadQuestions

--- CommandProjectP22/Source.cpp
+++ CommandProjectP22/Source.cpp
@@ -1,15 +1,38 @@
 #include<iostream>
+#include<io.h>
 using namespace std;
+
+void LoadQuestions()
+{
+	_finddata_t fileinfo;
+	intptr_t ptr;
+	int res = 0;
+
+	if ((ptr = _findfirst("Questions\\*.txt", &fileinfo))!=-1)
+	{
+		while (!res)
+		{
+			cout << fileinfo.name<<endl;
+
+			res = _findnext(ptr, &fileinfo);
+		}
+	}
+
+	_findclose(ptr);
+}
 
 void Victorina()
 {
+	cout << "Victorina" << endl << endl;
 
+	LoadQuestions();
 }
+
 
 int main()
 {
 	
-
+	Victorina();
 
 
 }
